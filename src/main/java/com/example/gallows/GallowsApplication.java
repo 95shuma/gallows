325eb008e9a5bf34d4e0782c736578ab9cf5eb93
95shuma <95shuma@gmail.com>
2020-08-19 11:38:33 +0600

    Initial commit

---
+++ src/main/java/com/example/gallows/GallowsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gallows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GallowsApplication.class, args);
+    }
+
+}
