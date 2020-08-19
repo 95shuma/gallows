efb83ff6bce926a1a91a1bd7a0c261871d9983ed
95shuma <95shuma@gmail.com>
2020-08-19 20:09:34 +0600

    add intermediate class UserPoint

---
+++ src/main/java/com/example/gallows/model/UserPoint.java
@@ -0,0 +1,11 @@
+package com.example.gallows.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserPoint {
+    private String name;
+    private Integer point;
+}
