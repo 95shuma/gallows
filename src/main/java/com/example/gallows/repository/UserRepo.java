33fb3686586afa2b8b3f74cfe030cf24bae9d9dc
95shuma <95shuma@gmail.com>
2020-08-19 15:14:37 +0600

    some change UserRepo

--- src/main/java/com/example/gallows/repository/UserRepo.java
+++ src/main/java/com/example/gallows/repository/UserRepo.java
@@ -3,5 +3,9 @@ package com.example.gallows.repository;
 import com.example.gallows.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo  extends JpaRepository<User, Integer> {
+
+    Optional<User> findUserByName(String name);
 }
