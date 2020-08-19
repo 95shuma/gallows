e4b261f520a12743320991147d585da54b312caf
95shuma <95shuma@gmail.com>
2020-08-19 15:12:42 +0600

    create class LoginController

---
+++ src/main/java/com/example/gallows/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.example.gallows.controller;
+
+import com.example.gallows.dto.UserDTO;
+import com.example.gallows.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+@AllArgsConstructor
+public class LoginController {
+    private UserService userService;
+
+    @GetMapping("/")
+    public String getMainPage(Model model, Principal principal) {
+        UserDTO userDTO = userService.getUserByName(principal.getName());
+        model.addAttribute("person",userDTO);
+        if (userDTO.getRole().equals("ROLE_ADMIN")){
+            model.addAttribute("admin", true);
+        } else {
+            model.addAttribute("user", true);
+        }
+        return "main";
+    }
+}
